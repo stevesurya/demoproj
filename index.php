bd58f2cd049fa924bc453f3a55c705809e229a1b
Suraj <suraj02@gmail.com>
2013-04-13 12:18:45 +0530

    commiting for r2

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	Author			:		Steve Surya
 	Date			:		Apr 13,2013
 	Purpose			:		This file would ionitialize the site
-	Version			:		1.0
+	Version			:		2.0
 */
 //integrate contact file
+//adding new features
 ?>
