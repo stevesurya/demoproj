56514f000dea48fa8bf8f0caa1a517abc6e91e22
Suraj <suraj02@gmail.com>
2013-04-13 12:22:32 +0530

    fixed the bug on master

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	Author			:		Steve Surya
 	Date			:		Apr 13,2013
 	Purpose			:		This file would ionitialize the site
-	Version			:		1.0
+	Version			:		1.1
 */
 //integrate contact file
+//version 1 fixes
 ?>
