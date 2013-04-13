bc50f3e1bd94b455a6718f41ee1681ff46f79ca2
Steve <steve.surya@ithands.com>
2013-04-13 11:57:39 +0530

    My initial commit

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+/*
+****************************************************
+	Name			:		contact.php
+	Author			:		Steve Surya
+	Date			:		Apr 13,2013
+	Purpose			:		This file would ionitialize the site
+	Version			:		1.0
+*/	
+
+
+	
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*****************************************************
+	Name			:		index.php
+	Author			:		Steve Surya
+	Date			:		Apr 13,2013
+	Purpose			:		This file would ionitialize the site
+	Version			:		1.0
+*/
+
+?>
